93a17510ee9e676649882333ce4c4ec061cd2ce2
Ritesh@1509 <kumaragarwalritesh@gmail.com>
2023-09-28 14:51:25 +0530

    ecommerce ui pages with router navgation

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import ProductDetailPage from "./pages/ProductDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "signup",
+    element: <SignupPage />,
+  },
+  {
+    path: "cart",
+    element: <CartPage />,
+  },
+  {
+    path: "checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "product-detail",
+    element: <ProductDetailPage />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
